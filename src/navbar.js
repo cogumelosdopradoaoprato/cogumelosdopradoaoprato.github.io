0217891f7f107f8dbdae04b0fe1fc34312219f23
Oversized Whisper <lunatese0907@gmail.com>
2025-05-03 23:49:52 +0100

    mushroom mobile + visuls + colors + code organizing + sponsors

--- src/navbar.js
+++ src/navbar.js
@@ -12,16 +12,16 @@ hamburger.addEventListener("click", () => {
     gsap.to(sideMenu, { x: 0, duration: 0.5, ease: "power2.out" });
 
     // Morph hamburger into X
-    gsap.to(topBar, { rotation: 45, y: 10, backgroundColor: "#fff", duration: 0.3 });
-    gsap.to(bottomBar, { rotation: -45, y: -8, backgroundColor: "#fff", duration: 0.3 });
+    gsap.to(topBar, { rotation: 45, y: 10, color:"#FA812F", duration: 0.3 });
+    gsap.to(bottomBar, { rotation: -45, y: -8,  color:"#FA812F", duration: 0.3 });
     gsap.to(middleBar, { opacity: 0, duration: 0.2 });
   } else {
     // Close menu
     gsap.to(sideMenu, { x: "100%", duration: 0.5, ease: "power2.in" });
 
     // Morph X back into hamburger
-    gsap.to(topBar, { rotation: 0, y: 0, backgroundColor: "#333", duration: 0.3 });
-    gsap.to(bottomBar, { rotation: 0, y: 0, backgroundColor: "#333", duration: 0.3 });
+    gsap.to(topBar, { rotation: 0, y: 0,  color:"#FA812F", duration: 0.3 });
+    gsap.to(bottomBar, { rotation: 0, y: 0, color:"#FA812F", duration: 0.3 });
     gsap.to(middleBar, { opacity: 1, duration: 0.2 });
   }
   menuOpen = !menuOpen;
